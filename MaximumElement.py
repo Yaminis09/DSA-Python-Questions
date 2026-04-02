515467e4d6ed409c0815ef52be41ec7655338b1e
Yamini Singh <123017653+Yaminis09@users.noreply.github.com>
2026-04-02 22:03:06 +0530

    Refactor max_element for safer initial max assignment
    Updated max_element function to improve safety in determining the maximum element.

--- MaximumElement.py
+++ MaximumElement.py
@@ -1,10 +1,10 @@
 arr = [-3, 7, 2, 9, 4]
 
 def max_element(arr):
-    element_max = arr[0]
+    element_max = arr[0] # Don't put 0, as in some hidden case we don't know what numbers are there. Safer to assume first number is the maximum.
     for i in range(len(arr)):
         
         if arr[i]>element_max:
             element_max = arr[i]
-    print(element_max)
+    #print(element_max)
     return element_max
